ecbec18ba4c19a6e78b21dc603ced59571d674b4
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-08-11 10:48:02 +0100

    fb scraper

--- backend/generate/views.py
+++ backend/generate/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from urllib.request import urlopen
-
+import mechanize
 # Create your views here.
 
 def facebook(request):
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    cookies = mechanize.CookieJar()
+    browser.set_cookiejar(cookies)
+    browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11;U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
+    browser.set_handle_refresh(False)
     url ="https://facebook.com"
+    browser.open(url)
+    browser.select_form(nr=0)
+    browser.form['email'] = "fb username"
+    browser.form['pass'] ="fb pass"
+    response = browser.submit()
+    print("fb res", response.read())
+
     page = urlopen(url)
     html_bytes = page.read()
     html = html_bytes.decode('utf-8')
     title = html.find('<title>')
-    print("itl", title)
+    # print("itl", title)
     start_index = title + len('<title')
-    print("star index", start_index)
+    # print("star index", start_index)
     end_index = html.find('</title>')
     title = html[start_index:end_index]
 
