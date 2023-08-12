3cee5117805698daca159df254d0b6a768ee7495
Fatai Akeem <akeemtolanifatai@gmail.com>
2023-08-12 16:15:06 +0100

    fb scraper function in progress

--- backend/generate/views.py
+++ backend/generate/views.py
@@ -11,25 +11,42 @@ def facebook(request):
     browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11;U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
     browser.set_handle_refresh(False)
     url ="https://facebook.com"
+    
+    # login to facebook
     browser.open(url)
     browser.select_form(nr=0)
     browser.form['email'] = ""
     browser.form['pass'] =""
     response = browser.submit()
-    print("fb res", response.geturl())
 
+    # click okay after login 
     browser.open(response.geturl())
     browser.select_form(nr=0)
     res = browser.submit()
-   
-    page = urlopen(url)
-    html_bytes = page.read()
-    html = html_bytes.decode('utf-8')
-    title = html.find('<title>')
-    # print("itl", title)
-    start_index = title + len('<title')
-    # print("star index", start_index)
-    end_index = html.find('</title>')
-    title = html[start_index:end_index]
 
-    return render(request, 'fb/fb.html', {'title':res.read()})
+    # search people
+    searchparam = "Fatai Akeem"
+    url = 'https://facebook.com/search_results/?q=Fatai+Akeem'
+    browser.open(url)
+    browser.find_link(text="See all")
+    req=browser.click_link(text="See all")
+    page = browser.open(req)
+    res = page.read()
+    # browser.close()
+
+    # click on see all to extract
+    # browser.open()
+
+    # page = urlopen(url)
+   
+    # page = urlopen(url)
+    # html_bytes = page.read()
+    # html = html_bytes.decode('utf-8')
+    # title = html.find('<title>')
+    # # print("itl", title)
+    # start_index = title + len('<title')
+    # # print("star index", start_index)
+    # end_index = html.find('</title>')
+    # title = html[start_index:end_index]
+
+    return render(request, 'fb/fb.html', {'title':res})
